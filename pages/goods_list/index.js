d6a7da784fde8e6884543a511ce9bf39a2193b59
梁彬熙 <liangbinxi@liangbinxideMacBook-Pro.local>
2019-08-03 02:42:28 +0800

    商品列表-自定义组件tabs

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,66 +1,43 @@
-// pages/goods_list/index.js
+import { request } from "../../request/index.js"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    tabs:[
+      {id:0,title:"综合",isActive:true},
+      {id:1,title:"销量",isActive:false},
+      {id:2,title:"综合",isActive:false}
+      ],
+    goodsList:[]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  queryParam :{
+    //搜索关键字
+    query:"",
+    //分类id
+    cid:"",
+    //页码
+    pagenum:1,
+    //页容量
+    pagesize:5
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onLoad(e){
+    // console.log(e)
+    // console.log(this)
+    this.queryParam.cid = e.id;
+    this.getGoodsList()
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  getGoodsList(){
+    request({url:"/goods/search",data:this.queryParam})
+    .then(res=>{
+      // console.log(res)
+      this.setData({
+        goodsList : res.goods
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleItemchange(e){
+    // console.log(e)
+    const {index} = e.detail;
+    let {tabs} = this.data;
+    tabs.forEach((v,i) => i ===index?v.isActive=true:v.isActive=false );
+    this.setData({tabs})
   }
 })
