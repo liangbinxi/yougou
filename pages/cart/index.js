8be651e93009d5e76a2e5029ed858c593cd62901
梁彬熙 <liangbinxi@liangbinxideMacBook-Pro.local>
2019-08-04 20:57:44 +0800

    获取用户收获地址

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,66 +1,48 @@
-// pages/cart/index.js
+import { getSetting, chooseAddress, openSetting } from '../../utils/asyncwx.js';
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  async handleChooseAddress(){
+    // wx.getSetting({
+    //   success: (result1) => {
+    //     console.log(result1)
+    //     const scopeAddress = result1.authSetting["scope.address"]
+    //     console.log(scopeAddress)
+    //     if(scopeAddress===true || scopeAddress===undefined){
+    //       wx.chooseAddress({
+    //         success (res) {
+    //           console.log(res.userName)
+    //           console.log(res.postalCode)
+    //           console.log(res.provinceName)
+    //           console.log(res.cityName)
+    //           console.log(res.countyName)
+    //           console.log(res.detailInfo)
+    //           console.log(res.nationalCode)
+    //           console.log(res.telNumber)
+    //         }
+    //       })
+    //     }else{
+    //       wx.openSetting({
+    //         success: (result2) => {
+    //           console.log(result2)
+    //         }
+    //       });
+            
+    //     }
+    //   }
+    // });
+    const res1 = await getSetting();
+    const scopeAddress = res1.authSetting["scope.address"];
+    console.log(scopeAddress)
+    if(scopeAddress===true || scopeAddress=== undefined){
+      const res2 = await chooseAddress();
+      console.log(res2);
+    }else{
+      await openSetting();
+      const res3 = await chooseAddress();
+      console.log(res3)
+    }
   }
 })
---
+++ utils/asyncwx.js
@@ -0,0 +1,45 @@
+
+export const openSetting = () =>{
+    return new Promise((resolve,reject)=>{
+        wx.openSetting({
+            success: (result) => {
+                resolve(result)
+            },
+            fail: (err) => {
+                reject(err)
+            },
+            complete: () => {}
+        });
+          
+    })
+}
+
+export const getSetting = function(){
+    return new Promise((resolve,reject)=>{
+        wx.getSetting({
+            success: (result) => {
+                resolve(result)
+            },
+            fail: (err) => {
+                reject(err)
+            },
+            complete: () => {}
+        });
+          
+    })
+}
+
+export const chooseAddress = () =>{
+    return new Promise((resolve,reject)=>{
+        wx.chooseAddress({
+            success: (result) => {
+                resolve(result)
+            },
+            fail: (err) => {
+                reject(err)
+            },
+            complete: () => {}
+        });
+          
+    })
+}
