3ac8e6c71151abd89b31e38494f608f4abceff1a
梁彬熙 <liangbinxi@liangbinxideMacBook-Pro.local>
2019-08-04 21:30:16 +0800

    获取显示收货地址

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -2,47 +2,27 @@ import { getSetting, chooseAddress, openSetting } from '../../utils/asyncwx.js';
 import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
   data: {
-
+    address:{}
   },
   async handleChooseAddress(){
-    // wx.getSetting({
-    //   success: (result1) => {
-    //     console.log(result1)
-    //     const scopeAddress = result1.authSetting["scope.address"]
-    //     console.log(scopeAddress)
-    //     if(scopeAddress===true || scopeAddress===undefined){
-    //       wx.chooseAddress({
-    //         success (res) {
-    //           console.log(res.userName)
-    //           console.log(res.postalCode)
-    //           console.log(res.provinceName)
-    //           console.log(res.cityName)
-    //           console.log(res.countyName)
-    //           console.log(res.detailInfo)
-    //           console.log(res.nationalCode)
-    //           console.log(res.telNumber)
-    //         }
-    //       })
-    //     }else{
-    //       wx.openSetting({
-    //         success: (result2) => {
-    //           console.log(result2)
-    //         }
-    //       });
-            
-    //     }
-    //   }
-    // });
+    
     const res1 = await getSetting();
     const scopeAddress = res1.authSetting["scope.address"];
     console.log(scopeAddress)
     if(scopeAddress===true || scopeAddress=== undefined){
-      const res2 = await chooseAddress();
-      console.log(res2);
+      
+        
     }else{
       await openSetting();
-      const res3 = await chooseAddress();
-      console.log(res3)
     }
+    const res2 = await chooseAddress();
+      console.log(res2);
+      res2.all = res2.provinceName+res2.cityName+res2.countyName+res2.detailInfo;
+      wx.setStorageSync("address", res2);
+  },
+  onShow(){
+    this.setData({
+      address : wx.getStorageSync("address") || {}
+    })
   }
 })
